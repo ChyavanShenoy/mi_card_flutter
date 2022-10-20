6138edd3fa8672d54752a4bc248316f674270972
Chyavan Shenoy <32121448+ChyavanShenoy@users.noreply.github.com>
2022-10-20 22:29:20 +0530

    Initial design

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/pfp.jpg'),
+            ),
+            Text(
+              "Chyavan Shenoy",
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        )),
+      ),
+    ),
+  );
+}
